152538fee9f68f033d642db226fc45926c862769
Dmitrii Borisov <glenstorm87@gmail.com>
2026-08-08 20:31:39 +0400

    improve data view

--- box-plots.py
+++ box-plots.py
@@ -11,10 +11,13 @@ app.layout = html.Div(
     [
         html.H4("Analysis of the rent statistics in Yerevan"),
         html.P("x-axis:"),
-        dcc.Checklist(
+        dcc.RadioItems(
             id="box-plots-x-x-axis",
-            options=["room_num", "region_id"],
-            value=["room_num"],
+            options=[
+                {"label": "room_num", "value": "room_num"},
+                {"label": "region", "value": "region_name"},
+            ],
+            value="room_num",
             inline=True,
         ),
         html.P("y-axis:"),
@@ -36,9 +39,34 @@ app.layout = html.Div(
 )
 def generate_chart(x, y):
     with sqlite3.connect("real_estate.db") as connection:
-        df = pd.read_sql_query("SELECT * FROM real_estate", connection)
+        df = pd.read_sql_query(
+            """
+            SELECT r.*, g.region_name
+            FROM REAL_ESTATE r
+            LEFT JOIN REGION g ON g.id = r.region_id
+            """,
+            connection,
+        )
+
+    # Keep district order from REGION.id
+    if x == "region_name":
+        region_order = (
+            df[["region_id", "region_name"]]
+            .drop_duplicates()
+            .sort_values("region_id")["region_name"]
+            .tolist()
+        )
+        fig = px.box(
+            df,
+            x=x,
+            y=y,
+            category_orders={"region_name": region_order},
+            width=1600,
+            height=1600,
+        )
+    else:
         fig = px.box(df, x=x, y=y, width=1600, height=1600)
-        return fig
+    return fig
 
 
 if __name__ == "__main__":
